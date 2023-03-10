2b563107fef5610777bc48bd451d527585a1aeea
Gerardo Quintero <jerryquinteroIII@Andrews-Mini.attlocal.net>
2023-03-10 16:36:15 -0700

    Hibernate

---
+++ JPAPokemon/src/test/java/com/skilldistillery/pokemon/entities/PokemonTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.pokemon.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PokemonTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Pokemon pokemon;
+	
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAPokemon");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		pokemon = em.find(Pokemon.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		assertNotNull(pokemon);
+		assertEquals("Pikachu", pokemon.getName());
+	}
+
+}
